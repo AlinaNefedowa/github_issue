c4e6d0cb0895c65f399d63fff06244ccbf88cb81
alinanefedowa <nefedowa.as@gmail.com>
2022-04-18 17:26:53 +0300

    WebSteps were written

--- src/test/java/guru/qa/LambdaWithSteps.java
+++ src/test/java/guru/qa/LambdaWithSteps.java
@@ -9,22 +9,36 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class LambdaWithSteps {
+
+        private static final String repository = "AlinaNefedowa/github_issue";
+        private static final int issue_number = 1;
 
         @Test
         public void testIssueSearch() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        open("https://github.com");
+                step("Open the main page", () -> {
+                        open("https://github.com");
+                });
 
-        $(".header-search-input").click();
-        $(".header-search-input").sendKeys("AlinaNefedowa/github_issue");
-        $(".header-search-input").submit();
+                step("Search for a repository " + repository, () -> {
+                        $(".header-search-input").click();
+                        $(".header-search-input").sendKeys(repository);
+                        $(".header-search-input").submit();
+                });
 
-        $(By.linkText("AlinaNefedowa/github_issue")).click();
-        $(By.partialLinkText("Issues")).click();
-        $(withText("#1")).should(Condition.exist);
+                step("Open the repository " + repository, () -> {
+                        $(By.linkText(repository)).click();
+                });
+                step("Go to the issue tab", () -> {
+                        $(By.partialLinkText("Issues")).click();
+
+                });
+                step("Check that the issue " + issue_number + " exists", () -> {
+                        $(withText("#" + issue_number)).should(Condition.exist);
+                });
         }
-
 }
---
+++ src/test/java/guru/qa/WebSteps.java
@@ -0,0 +1,40 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebSteps {
+
+    @Step("Open the main page")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Search for a repository {repository}")
+    public void searchForRepository(String repository) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repository);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Open the repository {repository}")
+    public void openRepository(String repository) {
+        $(By.linkText(repository)).click();
+    }
+
+    @Step("Go to the issue tab")
+    public void goToIssueTab() {
+        $(By.partialLinkText("Issues")).click();
+    }
+
+    @Step("Check that the issue exists {number}")
+    public void checkThatIssueExists(int number) {
+        $(withText("#" + number)).should(Condition.exist);
+    }
+
+}
