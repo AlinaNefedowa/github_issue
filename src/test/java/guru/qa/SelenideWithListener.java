02c9166b0ca5c184c86ce92734e2e4c2ca7bf08b
alinanefedowa <nefedowa.as@gmail.com>
2022-04-18 19:52:18 +0300

    updates

--- src/test/java/guru/qa/SelenideWithListener.java
+++ src/test/java/guru/qa/SelenideWithListener.java
@@ -2,7 +2,9 @@ package guru.qa;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -13,6 +15,12 @@ import static com.codeborne.selenide.Selenide.open;
 public class SelenideWithListener {
 
         @Test
+        @Owner("AlinaNefedowa")
+        @Severity(SeverityLevel.NORMAL)
+        @Link(value="GitHub",url = "https://github.com")
+        @Feature("Issues in repository")
+        @Story("The issue in repository should be visible")
+        @DisplayName("Тест на проверку вкладки issue")
         public void testIssueSearch() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
