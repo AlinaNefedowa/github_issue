6ee958f814f4dddca86a1aaeac6e403c9af90b73
alinanefedowa <nefedowa.as@gmail.com>
2022-04-18 19:15:58 +0300

    AnnotatedStepTests were written, build.gradle: aspectjVersion added

--- src/test/java/guru/qa/AnnotatedStepTests.java
+++ src/test/java/guru/qa/AnnotatedStepTests.java
@@ -1,4 +1,24 @@
 package guru.qa;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
 public class AnnotatedStepTests {
+
+    private static final String repository = "AlinaNefedowa/github_issue";
+    private static final int issue_number = 1;
+
+    @Test
+    public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        WebSteps steps = new WebSteps();
+        steps.openMainPage();
+        steps.searchForRepository(repository);
+        steps.openRepository(repository);
+        steps.goToIssueTab();
+        steps.checkThatIssueExists(issue_number);
+
+    }
 }
